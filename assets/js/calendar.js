f0dd4e2cf432011007f61e8c518ba4e146bffd12
MLOKLI <45224063+MLOKLI@users.noreply.github.com>
2020-08-31 17:05:54 +0300

    Update calendar.js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -32,7 +32,7 @@ function getData(data) {
 
 let table = '';
 $.ajax({
-    url: quarterUrl[data-1]//'https://script.google.com/macros/s/AKfycbz4D_xFAYYJxNF6FJeC95gRH9vdJSGh_d7cHb0TdpYGJRLjI1Us/exec'
+    url: quarterUrl[data]//'https://script.google.com/macros/s/AKfycbz4D_xFAYYJxNF6FJeC95gRH9vdJSGh_d7cHb0TdpYGJRLjI1Us/exec'
 }).then(function (result) {
     //console.log(result['result'][1][0])
     //console.log(result['result'][1][22])
