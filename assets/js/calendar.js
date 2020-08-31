d07ed4eab271e92ce4ae5f28975f9d14f5ea6497
MLOKLI <45224063+MLOKLI@users.noreply.github.com>
2020-08-31 17:37:24 +0300

    Update calendar.js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -38,11 +38,11 @@ let table = '';
 $.ajax({
     url: quarterUrl[data-1]//'https://script.google.com/macros/s/AKfycbz4D_xFAYYJxNF6FJeC95gRH9vdJSGh_d7cHb0TdpYGJRLjI1Us/exec'
 }).then(function (result) {
-    //console.log(result['result'][1][0])
-    //console.log(result['result'][1][22])
-    //console.log(result['result'][2][0])
-    //console.log(result['result'][i].indexOf($( "#family" ).val()));
-    //console.log(result['result'][i].search($( "#family" ).val()));
+    console.log(result['result'][1][0])
+    console.log(result['result'][1][22])
+    console.log(result['result'][2][0])
+    console.log(result['result'][i].indexOf($( "#family" ).val()));
+    console.log(result['result'][i].search($( "#family" ).val()));
     let numberUser = null;
     let month = 0;
     let statusJob = '';
